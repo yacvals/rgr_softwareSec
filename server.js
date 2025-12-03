645625bb4da62af5847e9736f8438d89e5dc401f
Vasiliadi Olesia <olesia@Vasiliadis-MacBook-Air.local>
2025-12-03 11:39:48 +0200

    feat: generate RSA keypair on server and send publicKey

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const net = require('net');
+const crypto = require('crypto');
 
 const PORT = 8080;
 
+const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+  modulusLength: 2048,
+  publicKeyEncoding: { type: 'spki', format: 'pem' },
+  privateKeyEncoding: { type: 'pkcs1', format: 'pem' }
+});
+
+console.log('[server] RSA keys generated');
+
 const server = net.createServer((socket) => {
   console.log('[server] client connected');
-
+  
   socket.on('data', (data) => {
     console.log('[server] received:', data.toString());
+    // Відправляємо публічний ключ
+    socket.write(publicKey);
   });
-
-  socket.on('end', () => {
-    console.log('[server] client disconnected');
-  });
+  
+  socket.on('end', () => console.log('[server] client disconnected'));
 });
 
 server.listen(PORT, () => {
